2bfc6f0d62ec08a548aa28de1190a59ffef55b09
ikegaya-h <37140238+ikegaya-h@users.noreply.github.com>
2021-12-26 17:47:30 +0900

    Add: 処理の方針を追加

--- app.rb
+++ app.rb
@@ -23,11 +23,27 @@ post '/callback' do
     when Line::Bot::Event::Message
       case event.type
       when Line::Bot::Event::MessageType::Text
+        #クイックリプライにセットしたメッセージと一致しているか確認
+          #やりとりの最後なら終了メッセージを送信
+          #クイックリプライと一致していなければメッセージを再送
+        #どこまでやり取りしたかをDBに保存
+        #返信メッセージを生成
         message = {
           type: 'text',
           text: event.message['text']
         }
         client.reply_message(event['replyToken'], message)
+      when Line::Bot::Event::MessageType::File
+        #ファイル名が.txtになっているか確認
+        #ファイルのIDでLINEサーバからtxtデータを取得する
+          #文字のエンコードによっては文字化けするかも？
+        response = client.get_message_content(event.message['id'])
+        tf = Tempfile.open("content")
+        #取得したtxtファイルを加工
+        #クイックリプライにクライアントが過去に送信したのメッセージを入れる
+        #一番初めのメッセージを送信
+          #初めのメッセージがクライアントの場合"スタート"のメッセージから始める
+
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
         tf = Tempfile.open("content")
