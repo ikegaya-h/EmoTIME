a9e791053ecbb8bde3565ff2591356642a1f33fc
ikegaya-h <37140238+ikegaya-h@users.noreply.github.com>
2021-12-28 04:22:09 +0900

    Add: 処理の内容をメモ

--- app.rb
+++ app.rb
@@ -10,18 +10,18 @@ def client
 end
 
 post '/callback' do
-  body = request.body.read
+  body = request.body.read #送られてきたJSONを開いてbodyに格納
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
   end
 
-  events = client.parse_events_from(body)
-  events.each do |event|
+  events = client.parse_events_from(body) #bodyの中身(JSON)をイベントだけ取り出してhashにする
+  events.each do |event| #文字通り送られたイベントhashを一つづつ入れている
     case event
-    when Line::Bot::Event::Message
-      case event.type
+    when Line::Bot::Event::Message #eventの文字とモジュールの定数が一致しているか比較している
+      case event.type #セッターとゲッターを自動で定義するメソッドによって中身の読み書きができる
       when Line::Bot::Event::MessageType::Text
         #クイックリプライにセットしたメッセージと一致しているか確認
           #やりとりの最後なら終了メッセージを送信
@@ -35,9 +35,10 @@ post '/callback' do
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::File
         #ファイル名が.txtになっているか確認
+        
         #ファイルのIDでLINEサーバからtxtデータを取得する
           #文字のエンコードによっては文字化けするかも？
-        response = client.get_message_content("<messageId>")
+        response = client.get_message_content("<messageId>") #引数にevent.message['id']を指定することでURI生成＋ファイルをGETリクエスト
         case response
         when Net::HTTPSuccess then
           tf = Tempfile.open("content")
