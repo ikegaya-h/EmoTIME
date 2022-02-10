562c3fd8ad867834bd17314eb148448206b37376
ikegaya-h <37140238+ikegaya-h@users.noreply.github.com>
2022-02-11 06:36:14 +0900

    Fix: 大文字を避けてデフォルト値を設定

--- db/migrate/20220207060547_create_users.rb
+++ db/migrate/20220207060547_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :user_id,          null: false, unique: true
-      t.string :official_title,   null: false, default: 'OfficialTitle'
+      t.string :official_title,   null: false, default: 'emotime'
       t.integer :resending_point
       t.integer :replay_point
       t.string :file_id
