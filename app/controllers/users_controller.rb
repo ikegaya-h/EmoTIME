8e9e2ba02e3714e2d443914e0469d3f480784503
ikegaya-h <37140238+ikegaya-h@users.noreply.github.com>
2022-02-15 01:51:42 +0900

    Fix: MessageType::Textも修正された配列の整形処理に準拠するように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,79 +27,97 @@ class UsersController < ApplicationController
           #ユーザーを取得
           user = User.find_by!(user: client.channel_id)
           #ファイルのIDでLINEサーバからtxtデータを取得する
-          txt = client.get_message_content(user.file_id)
-          #CompatibilityError回避のため正規表現と同じエンコードを指定
-          txt.map {|n| n.force_encoding('utf-8') }
-          #配列を整形
-            #保存日時と改行のみの行を削除
-          txt.each do |s|
-            if "\r" == s
-              txt.delete(s)
-            elsif /保存日時：20[0-9][0-9]\/[01][0-2]\/[0-3][0-9] [0-2][0-9]:[0-5][0-9]/ === s
-              txt.delete(s)
-            end
-          end
-          #時系列と発言者と発言内容を二重配列に整頓
-          #行が変化する加工が完了したら実行
-          count = 0
-          txt.each do |s|
-            if /[0-2][0-9]:[0-5][0-9]/ === s
-              txt[count] = s.split(/\t/)
-            end
-            count += 1
-          end
-          #クイックリプライにセットしたメッセージと一致しているか確認
-            #やりとりの最後なら終了メッセージを送信
-            #クイックリプライと一致していなければメッセージを再送
-          count = user.replay_point
-          if client.reply_message == txt[user.resending_point][1]
-            while txt[count][1] == user.official_title do
-              send_message += "#{txt[count][2]}\n"
-              count += 1
-            end
-            until txt[count][1] == user.official_title do
-              set_message += "#{txt[count][2]}\n"
-              count += 1
-            end
-            user.resending_point = user.replay_point
-            unless set_message
-              set_message = "~end~"
-              user.replay_point = 2
-            end
-            #どこまでやり取りしたかをDBに保存
-            user.save!
-          else
-            count = user.resending_point
-            while txt[count][1] == user.official_title do
-              send_message += "#{txt[count][2]}\n"
-              count += 1
-            end
-            until txt[count][1] == user.official_title do
-              set_message += "#{txt[count][2]}\n"
-              count += 1
-            end
-          end
-          #返信メッセージを生成
-          message = {
-            type: 'text',
-            text: send_message,
-            sender: {
-              name: user.official_title
-            },
-            quickReply: {
-              items: [
-                {
-                  type: "action",
-                  action: {
-                    type: "message",
-                    label: "返信",
-                    text: set_message
-                  }
-                }
-              ]
+          # @return [Net::HTTPResponse]
+          response = client.get_message_content(user.file_id)
+          case response
+          when Net::HTTPSuccess then
+            #response.body -> Strings
+            txt = []
+            response.body.each_line { |line|
+            txt << line.gsub!(/\n/) { '' }
             }
-          }
-          client.reply_message(event['replyToken'], message)
+            #CompatibilityError回避のため正規表現と同じエンコードを指定
+            txt.map {|n| n.force_encoding('utf-8') }
+            #配列を整形
+            txt[0] = txt[0].delete("[LINE] ")
+            txt[0] = txt[0].delete("とのトーク履歴")
+            #保存日時と改行のみの行を削除
+            txt.each do |s|
+              if "" == s
+                txt.delete(s)
+              elsif /保存日時：20[0-9][0-9]\/[01][0-2]\/[0-3][0-9] [0-2][0-9]:[0-5][0-9]/ === s
+                txt.delete(s)
+              end
+            end
+            #時系列と発言者と発言内容を二重配列に整頓
+            #行が変化する加工が完了したら実行
+            count = 0
+            txt.each do |s|
+              case s
+              when /[0-2][0-9]:[0-5][0-9]/
+                txt[count].gsub!(/\"/) { '' }
+                txt[count] = s.split(/\t/)
+              when /\"/
+                previous = 0
+                previous = count - 1
+                txt[count] = [txt[previous][0], txt[previous][1], txt[count].gsub!(/\"/) { '' }]
+              end
+              count += 1
+            end
+            #クイックリプライにセットしたメッセージと一致しているか確認
+              #やりとりの最後なら終了メッセージを送信
+              #クイックリプライと一致していなければメッセージを再送
+            count = user.replay_point
+            if client.reply_message == txt[user.resending_point][1]
+              while txt[count][1] == user.official_title do
+                send_message += "#{txt[count][2]}\n"
+                count += 1
+              end
+              until txt[count][1] == user.official_title do
+                set_message += "#{txt[count][2]}\n"
+                count += 1
+              end
+              user.resending_point = user.replay_point
+              unless set_message
+                set_message = "~end~"
+                user.replay_point = 2
+              end
+              #どこまでやり取りしたかをDBに保存
+              user.save!
+            else
+              count = user.resending_point
+              while txt[count][1] == user.official_title do
+                send_message += "#{txt[count][2]}\n"
+                count += 1
+              end
+              until txt[count][1] == user.official_title do
+                set_message += "#{txt[count][2]}\n"
+                count += 1
+              end
+            end
+            #返信メッセージを生成
+            message = {
+              type: 'text',
+              text: send_message,
+              sender: {
+                name: user.official_title
+              },
+              quickReply: {
+                items: [
+                  {
+                    type: "action",
+                    action: {
+                      type: "message",
+                      label: "返信",
+                      text: set_message
+                    }
+                  }
+                ]
+              }
+            }
+            client.reply_message(event['replyToken'], message)
+          end
+        end
         when Line::Bot::Event::MessageType::File
           #ファイル名が.txtになっているか確認
           unless /とのトーク.txt/ === event["message"]["fileName"]
