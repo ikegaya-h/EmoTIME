ffcb1d87b222cc006cf7f6079e0851f96d940f13
ikegaya-h <37140238+ikegaya-h@users.noreply.github.com>
2022-02-27 01:51:47 +0900

    Fix: 保存日時を削除する処理を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -175,14 +175,13 @@ class UsersController < ApplicationController
             end
             # p txt
             txt.map { |n| n.gsub!(/\r\n/) { '' } }
+            txt.map { |n| n.gsub!(/保存日時：20[0-9][0-9]\/[01][0-2]\/[0-3][0-9] [0-2][0-9]:[0-5][0-9]/) { '' } }
             txt.map { |n| n.gsub!(/20[0-9][0-9]\/[01][0-2]\/[0-3][0-9]\((月|火|水|木|金|土|日)\)/) { '' } }
             # p txt
             txt[0] = txt[0].delete("[LINE] ")
             txt[0] = txt[0].delete("とのトーク履歴")
             txt.each do |s|
               if s == ""
-                txt.delete(s)
-              elsif /保存日時：20[0-9][0-9]\/[01][0-2]\/[0-3][0-9] [0-2][0-9]:[0-5][0-9]/ === s
                 txt.delete(s)
               end
             end
