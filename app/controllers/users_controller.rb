c9fb458c50c6c1ceaeff4939fb27e8e9b71f6961
ikegaya-h <37140238+ikegaya-h@users.noreply.github.com>
2022-02-26 23:20:15 +0900

    Fix: Fileの整形処理をTextに合わせる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -160,6 +160,7 @@ class UsersController < ApplicationController
             end
             p txt
             txt.map { |n| n.gsub!(/\r\n/) { '' } }
+            txt.map { |n| n.gsub!(/20[0-9][0-9]\/[01][0-2]\/[0-3][0-9]\((月|火|水|木|金|土|日)\)/) { '' } }
             p txt
             txt[0] = txt[0].delete("[LINE] ")
             txt[0] = txt[0].delete("とのトーク履歴")
@@ -169,9 +170,6 @@ class UsersController < ApplicationController
                 txt.delete(s)
               elsif /保存日時：20[0-9][0-9]\/[01][0-2]\/[0-3][0-9] [0-2][0-9]:[0-5][0-9]/ === s
                 txt.delete(s)
-              elsif /20[0-9][0-9]\/[01][0-2]\/[0-3][0-9]\((月|火|水|木|金|土|日)\)/ === s
-                txt.delete(s)
-              end
             end
             p txt
             count = 0
